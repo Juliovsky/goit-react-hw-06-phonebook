1ca834aa7048f7fb9918fb4df69c1f7526d0b0a2
Yuliia <ybelova24@gmail.com>
2020-05-22 17:36:20 +0300

    Updates

---
+++ precache-manifest.b845cbede214975ab5c708de60537143.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78f880b76627bbb00a5c256e1430ad9d",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "eae0e5cafe5e05644df0",
+    "url": "/goit-react-hw-06-phonebook/static/css/main.45849f93.chunk.css"
+  },
+  {
+    "revision": "af07e10ba61bf356fd2d",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.ef0ead9a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.ef0ead9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eae0e5cafe5e05644df0",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.63b9b37c.chunk.js"
+  },
+  {
+    "revision": "e5436373ee20b92c06ac",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.22e6677c.js"
+  }
+]);
